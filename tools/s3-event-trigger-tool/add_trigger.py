ccaf189d925c00feee3b820febf73d60ac086fd3
hawkey999 <hawkey99991@gmail.com>
2022-02-18 23:10:47 +0800

    fix annotation

--- tools/s3-event-trigger-tool/add_trigger.py
+++ tools/s3-event-trigger-tool/add_trigger.py
@@ -1,14 +1,14 @@
 
-"""" Notice:
+""" Notice:
 1. put_bucket_notification_configuration S3接口是每次写入都直接替换之前的notification_config，不能新增。
 2. 最大100条记录。
 3. By default, only the bucket owner can configure notifications on a bucket. However, bucket owners can use a bucket policy to grant permission to other users to set this configuration with s3:PutBucketNotification permission.
-""""
+"""
 from boto3.session import Session
 
 
 ### SETTING ###
-Bucket='hzb-s3-to-gcs'
+Bucket='hzb-gcs-test2'
 aws_profile_name='s3'  # aws cli profile in local PC
 sqs_arn='arn:aws:sqs:us-west-2:278484429967:s3_migration_file_list'
 prefix_file_name='prefix_list.txt'
--- tools/s3-event-trigger-tool/list_trigger.py
+++ tools/s3-event-trigger-tool/list_trigger.py
@@ -7,7 +7,7 @@ from boto3.session import Session
 
 
 ### SETTING ###
-Bucket='hzb-s3-to-gcs'
+Bucket='hzb-cross-owner2'
 aws_profile_name='s3'  # aws cli profile in local PC
 prefix_file_name='prefix_list_output.txt'
 ###############
@@ -18,8 +18,9 @@ client = Session(profile_name=aws_profile_name).client('s3')
 response = client.get_bucket_notification_configuration(Bucket=Bucket)
 prefix_list=[]
 for n in response['QueueConfigurations']:
+    preid = n['Id']
     prefix = n['Filter']['Key']['FilterRules'][0]['Value']
-    prefix_list.append(prefix)
+    prefix_list.append([preid, prefix])
 
 for p in prefix_list:
-    print(p)
+    print(p[0], p[1])
